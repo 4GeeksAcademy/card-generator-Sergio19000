91cb67174929acd4c336c957c710e27fba3c0272
sergio19000 <119664418+sergio19000@users.noreply.github.com>
2026-02-20 09:10:56 +0000

    subir cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 import "bootstrap";
 import "./style.css";
-
+import "./app.js";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    generarCartaAleatoria();
 };
+
+function generarCartaAleatoria() {
+    const palos = [
+        { nombre: "diamantes", simbolo:"♦"},
+        { nombre:"corazones" ,simbolo:"♥"},
+        { nombre:"picas" , simbolo:"♠"},
+        { nombre:"treboles"  ,simbolo:"♣"}
+    ];
+    const valores = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    
+    const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+    const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+
+    const carta =document.getElementById("carta");
+    const simboloAriba = document.getElementById("simbolo-arriba");
+    const simboloAbajo = document.getElementById("simbolo-abajo");
+    const valorCarta = document.getElementById("valor-carta");
+
+   
+    carta.className = "card " + paloAleatorio.nombre;
+
+
+
+    simboloAriba.innerHTML = paloAleatorio.simbolo;
+    simboloAbajo.innerHTML = paloAleatorio.simbolo;
+    valorCarta.innerHTML = valorAleatorio;
+
+
+
+}
